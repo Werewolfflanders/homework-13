197e2b9bec572db5538f356f07abe8539f658e0e
Christopher Wood <cjwood01@gmail.com>
2020-03-15 09:44:14 -0500

    got connection to burgers_db working. added api routes for creating and updating

---
+++ controllers/burgersController.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+  burger.all(data => {
+    const hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.create(["burger_name"], [req.body.burger_name], result => {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+  burger.update(
+    {
+      devoured: req.body.devour
+    },
+    condition,
+    result => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
+module.exports = router;
