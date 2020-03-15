e11a4fe897bf4421cf9abc5f6d1c81a59c1361e2
Christopher Wood <cjwood01@gmail.com>
2020-03-15 09:15:10 -0500

    made seeds.sql. added entries to burgers_db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Rodeo Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', true);
